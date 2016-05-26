796b53a339400573bd53ab3345090df06e0b9097
Benoît Sauvère <benoit.sauvere@teevity.com>
2016-05-26 16:05:51 +0200

    Partie 4 - Ajout des point d'entrée pour les différentes VM + Génération des jar avec maven

---
+++ src/main/java/m2dl/pcr/akka/partie3/CryptageProviderVMEntryPoint.java
@@ -0,0 +1,11 @@
+package m2dl.pcr.akka.partie3;
+
+/**
+ * Created by benoit on 26/05/2016.
+ */
+public class CryptageProviderVMEntryPoint {
+
+    public static void main(String[] args) {
+
+    }
+}
