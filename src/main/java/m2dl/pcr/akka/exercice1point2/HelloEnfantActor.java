d5f7c768367b01bf8e1619f1920d2da082d1c06e
Julien Custoja <julien.custoja@gmail.com>
2016-05-26 14:22:38 +0200

    Exercice 1.2 OK

---
+++ src/main/java/m2dl/pcr/akka/exercice1point2/HelloEnfantActor.java
@@ -0,0 +1,28 @@
+package m2dl.pcr.akka.exercice1point2;
+
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+
+public class HelloEnfantActor extends UntypedActor {
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+    public HelloEnfantActor() {
+        log.info("HELLOENFANT constructor");
+    }
+
+    @Override
+    public void onReceive(Object msg) throws Exception {
+
+        log.info("Received msg: " + msg);
+
+        if (msg instanceof String) {
+           log.info("Hello " + msg + "!");
+        } else {
+            unhandled(msg);
+        }
+    }
+
+
+}
