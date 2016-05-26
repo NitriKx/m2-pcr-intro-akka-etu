81969cf0de296153e6e48bc515b57b3e34c58c8d
Serut <l.mieulet@gmail.com>
2016-05-26 16:25:45 +0200

    Décryptage dans le récepteur

--- src/main/java/m2dl/pcr/akka/partie3/RecepteurActor.java
+++ src/main/java/m2dl/pcr/akka/partie3/RecepteurActor.java
@@ -3,6 +3,7 @@ package m2dl.pcr.akka.partie3;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import m2dl.pcr.akka.stringservices.StringUtils;
 
 import java.util.ArrayList;
 
@@ -19,7 +20,21 @@ public class RecepteurActor extends UntypedActor {
     @Override
     public void onReceive(Object msg) throws Exception {
         if (msg instanceof String) {
-            log.info("Message reçu : "+msg.toString());
+            Message message = (Message) msg;
+            // teste si le message est verifiable
+            String messageVerifie = StringUtils.verifieCtrl(message.getMessage());
+            String messageDecrypte;
+            if (messageVerifie == null) {
+                // un erreur controller provider a déjà enlevé le caractere
+                messageDecrypte = StringUtils.decrypte(message.getMessage());
+            } else {
+                messageDecrypte = StringUtils.decrypte(messageVerifie);
+            }
+            if (messageDecrypte != null) {
+                log.info("Message reçu : " + messageDecrypte);
+            } else {
+                log.error("Le message ne contient pas de ctrl");
+            }
         }
     }
 }
